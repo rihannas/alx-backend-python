44343f75d5606bbb10f716082007f2377f98bcaf
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-06-23 21:01:05 +0300

    task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+A type-annotated function floor which takes a float n as argument
+and returns the floor of the float.
+"""
+
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    function that performs a float division
+    and returns an int
+    """
+    return math.floor(n)
