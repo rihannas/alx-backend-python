1d6cc19fbf009192fa95ed11b81ac566414fa407
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-06-23 20:46:17 +0300

    task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+A type-annotated function concat that takes
+a string str1 and a string str2 as arguments
+and returns a concatenated string
+"""
+
+
+import string
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    a function that concatenate two strings
+    return type: string
+    """
+
+    return str1 + str2
