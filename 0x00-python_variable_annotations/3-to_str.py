7d0abdd3aa77c3439a8d689f820119b14df97312
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-06-23 21:07:48 +0300

    task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+A type-annotated function to_str that takes a float n as argument
+and returns the string representation of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    converts a float to string
+    returns a string
+    """
+    return str(n)
