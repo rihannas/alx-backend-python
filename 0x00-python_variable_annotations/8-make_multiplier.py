7300ad264a21716b0e706bcc6efaf913dcda3888
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-06-24 01:09:46 +0300

    task 8

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+""" type-annotated function make_multiplier that takes a float multiplier
+as argument and returns a function that multiplies a float by
+multiplier.
+"""
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Takes in a float and returns a float
+    """
+    def fn(n: float):
+        return n * multiplier
+    return fn
