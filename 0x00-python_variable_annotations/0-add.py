32a9dbdaff1c9fe73ee188794fb191e4271e78c9
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-06-23 20:41:00 +0300

    task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+A type-annotated function add that takes a float a and a float b as arguments
+and returns their sum as a float.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    a function that adds two floats
+    return type: float
+    """
+
+    return a + b
