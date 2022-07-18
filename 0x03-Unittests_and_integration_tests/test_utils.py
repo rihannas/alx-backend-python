a3f1c0b36b05a2c10d4a87538de0915edec40a15
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-07-18 14:59:10 +0300

    task 0

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Unittest for utils.access_nested_map"""
+
+import unittest
+from utils import access_nested_map
+
+from parameterized import parameterized
+
+""" Note: Parameterized tests allow a developer to run the same test over
+and over again using different values. """
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """class that test the function access_nested_map"""
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expexted_value):
+        """tests access_nested_map function"""
+
+        self.assertEqual(access_nested_map(nested_map, path), expexted_value)
+
+
+if __name__ == '__main__':
+    unittest.main()
