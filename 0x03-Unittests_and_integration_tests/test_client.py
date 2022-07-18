c42e2aa0a19affe0165d8719cec0ec50ab459460
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-07-18 20:23:01 +0300

    task 8

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,8 +3,9 @@
 
 import unittest
 from unittest.mock import Mock, patch, PropertyMock
-from parameterized import parameterized
+from parameterized import parameterized, parameterized_class
 from client import GithubOrgClient
+from fixtures import TEST_PAYLOAD
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -16,8 +17,12 @@ class TestGithubOrgClient(unittest.TestCase):
     ])
     @patch("client.get_json")
     def test_org(self, org_name, mock_json):
-        """
-        test that GithubOrgClient.org returns correct value.
+        """test_org- function to test the github org
+        Arguments:
+            org_name: the given name of the organization
+        Returns:
+            ok if it succedd fail otherwisetest that GithubOrgClient.org
+            returns correct value.
         """
         test_class = GithubOrgClient(org_name)
         test_class.org()
@@ -28,7 +33,13 @@ class TestGithubOrgClient(unittest.TestCase):
         ("random-url", {'repos_url': 'http://some_url.com'})
     ])
     def test_public_repos_url(self, name, result):
-        """tests memoize property """
+        """
+        test_public_repos_url - functin to test the public repos url
+        Arguments:
+            nothing
+        Returns:
+            ok if it succeded fail otherwise
+        """
 
         with patch('client.GithubOrgClient.org',
                    PropertyMock(return_value=result)):
@@ -74,3 +85,41 @@ class TestGithubOrgClient(unittest.TestCase):
         """
         tst_cls = GithubOrgClient.has_license(repo, license_key)
         self.assertEqual(tst_cls, rslt)
+
+
+@parameterized_class(
+    ('org_payload', 'repos_payload', 'expected_repos', 'apache2_repos'),
+    TEST_PAYLOAD
+)
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """an integration test for githuborg client"""
+
+    @classmethod
+    def setUpClass(cls):
+        """ set up class befor each method"""
+        config = {'return_value.json.side_effect':
+                  [
+                      cls.org_payload, cls.repos_payload,
+                      cls.org_payload, cls.repos_payload
+                  ]
+                  }
+        cls.get_patcher = patch('requests.get', **config)
+        cls.mock = cls.get_patcher.start()
+
+    def test_public_repos(self):
+        """add some more integration"""
+        tst_cls = GithubOrgClient('Facebook')
+        self.assertEqual(tst_cls.org, self.org_payload)
+        self.assertEqual(tst_cls.repos_payload, self.repos_payload)
+        self.assertEqual(tst_cls.public_repos(), self.expected_repos)
+        self.mock.assert_called()
+
+    def test_public_repos_with_license(self):
+        """ method to test the public_repos with the argument license """
+        test_class = GithubOrgClient("holberton")
+        assert True
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        """tear down after each class"""
+        cls.get_patcher.stop()
