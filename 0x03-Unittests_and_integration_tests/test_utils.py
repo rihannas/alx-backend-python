e03327779131a0f76f3fdcc7f1c99ca94eb27a75
Rihanna <75071112+avocadoclouds@users.noreply.github.com>
2022-07-18 15:05:43 +0300

    task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -16,12 +16,20 @@ class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2)
+        ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expexted_value):
         """tests access_nested_map function"""
 
         self.assertEqual(access_nested_map(nested_map, path), expexted_value)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """ Uses the assertRaises context manager to test that a KeyError is raised."""
+        self.assertRaises(KeyError, access_nested_map, nested_map, path)
 
 
 if __name__ == '__main__':
