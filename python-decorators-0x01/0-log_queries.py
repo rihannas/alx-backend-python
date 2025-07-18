0bc3e43cbf4e2a0b71fd15a1666d0a04ed7ea845
rihanas <rihanna.socials@gmail.com>
2025-07-18 23:55:56 +0300

    fix

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 '''create a decorator that logs database queries executed by any function'''
 
-import sqlite3
-import functools
-
 def log_queries(func):
     def wrapper(*args, **kwargs):
         print('Query Logs:')
@@ -11,14 +8,4 @@ def log_queries(func):
         func(*args, **kwargs)
     return wrapper
 
-@log_queries
-def fetch_all_users(query):
-    conn = sqlite3.connect('users.db')
-    cursor = conn.cursor()
-    cursor.execute(query)
-    results = cursor.fetchall()
-    conn.close()
-    return results
 
-#### fetch users while logging the query
-users = fetch_all_users(query="SELECT * FROM users")
