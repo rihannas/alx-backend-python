04226cad8a394d4e331677ff410101f17ac36877
rihanas <rihanna.socials@gmail.com>
2025-07-20 15:49:46 +0300

    update

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -16,40 +16,18 @@ class UserSerializer(serializers.ModelSerializer):
         ]
 
 
-class RegisterUserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-
-    class Meta:
-        model = User
-        fields = [
-            'email',
-            'first_name',
-            'last_name',
-            'password',
-            'phone_number',
-            'role',
-        ]
-
-    def create(self, validated_data):
-        user = User(
-            email=validated_data['email'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name'],
-            phone_number=validated_data.get('phone_number', ''),
-            role=validated_data.get('role', 'guest'),
-        )
-        user.set_password(validated_data['password'])
-        user.save()
-        return user
-
-
 class MessageSerializer(serializers.ModelSerializer):
+    sender = UserSerializer(read_only=True)  # nested user inside message
+
     class Meta:
         model = Message
-        fields = ['message_id', 'sender_id', 'message_body', 'sent_at']
+        fields = ['message_id', 'sender', 'message_body', 'sent_at']
 
 
 class ConversationSerializer(serializers.ModelSerializer):
+    participants = UserSerializer(many=True, read_only=True)  # nested users
+    messages = MessageSerializer(many=True, read_only=True)   # nested messages
+
     class Meta:
         model = Conversation
-        fields = ['conversation_id', 'participants_id', 'created_at']
+        fields = ['conversation_id', 'participants', 'messages', 'created_at']
