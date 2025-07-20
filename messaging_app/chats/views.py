e71d6e63b439add975bd816d45a5dd536bc6dd6b
rihanas <rihanna.socials@gmail.com>
2025-07-20 16:28:15 +0300

    add views

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status, filters
+from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError
 
 from .models import Conversation, Message, User
@@ -9,26 +10,35 @@ class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['conversation_id']
+    ordering = ['-conversation_id']
 
     def get_queryset(self):
-        # Only show conversations where the user is a participant
         return self.queryset.filter(participants__in=[self.request.user])
 
     def perform_create(self, serializer):
-        # Automatically add the current user as a participant
         participants = self.request.data.get('participants', [])
         if str(self.request.user.id) not in participants:
             participants.append(str(self.request.user.id))
         serializer.save(participants=participants)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['timestamp']
+    ordering = ['-timestamp']
 
     def get_queryset(self):
-        # Only show messages from conversations the user is part of
         return self.queryset.filter(conversation__participants__in=[self.request.user])
 
     def perform_create(self, serializer):
@@ -36,3 +46,9 @@ class MessageViewSet(viewsets.ModelViewSet):
         if self.request.user not in conversation.participants.all():
             raise ValidationError("You are not a participant in this conversation.")
         serializer.save(sender=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
