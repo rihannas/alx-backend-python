631aa0d9bae90fce98a11d57a8c83afaf97ebc90
rihanas <rihanna.socials@gmail.com>
2025-07-26 00:27:58 +0300

    update

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -130,7 +130,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',  
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'chats.auth.CustomJWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',     
     ],
 }
 
