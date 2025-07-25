b14a456cd4cb835644acabb3a6215be3cca71370
rihanas <rihanna.socials@gmail.com>
2025-07-26 00:52:27 +0300

    update

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -1,4 +1,4 @@
-# chats/pagination.py
+from rest_framework.response import Response
 
 from rest_framework.pagination import PageNumberPagination
 
@@ -6,3 +6,13 @@ class MessagePagination(PageNumberPagination):
     page_size = 20
     page_size_query_param = 'page_size'
     max_page_size = 100
+
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.page.paginator.count,     # 👈 total items
+            'total_pages': self.page.paginator.num_pages,
+            'current_page': self.page.number,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data
+        })
