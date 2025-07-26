b7f0ce7c42854888b8c117146e10a1c0db2faa6c
rihanas <rihanna.socials@gmail.com>
2025-07-26 19:30:57 +0300

    add middlewear

---
+++ Django-Middleware-0x03/chats/middleware.py
@@ -0,0 +1,19 @@
+import logging
+from datetime import datetime
+
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler('user_requests.log')
+formatter = logging.Formatter('%(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.INFO)
+
+class RequestLoggingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        user = request.user if request.user.is_authenticated else "Anonymous"
+        log_message = f"{datetime.now()} - User: {user} - Path: {request.path}"
+        logger.info(log_message)
+        return self.get_response(request)
