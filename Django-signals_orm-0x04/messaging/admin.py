37e44e61fdb9485758b89dcc9e67ac219454cd29
rihanas <rihanna.socials@gmail.com>
2025-08-03 22:07:31 +0300

    add task 0

---
+++ Django-signals_orm-0x04/messaging/admin.py
@@ -0,0 +1,15 @@
+# admin.py
+
+from django.contrib import admin
+from .models import Message, Notification
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'sender', 'receiver', 'timestamp')
+    search_fields = ('sender__username', 'receiver__username', 'content')
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'message', 'is_read', 'created_at')
+    list_filter = ('is_read',)
+    search_fields = ('user__username', 'message__content')
