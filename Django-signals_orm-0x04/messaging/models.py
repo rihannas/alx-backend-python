3bdb03de69c64506e2db920f85c507ec6184383e
rihanas <rihanna.socials@gmail.com>
2025-08-03 22:25:44 +0300

    task 1

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -28,6 +28,7 @@ class MessageHistory(models.Model):
     message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name="edit_history")
     old_content = models.TextField()
     edited_at = models.DateTimeField(auto_now_add=True)
+    edited_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
-        return f"History of message {self.message.id} at {self.edited_at}"
+        return f"History of message {self.message.id} edited by {self.edited_by}"
